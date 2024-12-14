6037b47dad22ab46ed636658622a378129b0735a
7azeemm <hazemfarah26@gmail.com>
2024-12-14 18:27:21 +0100

    test

--- src/main/java/com/hazem/skyplus/Skyplus.java
+++ src/main/java/com/hazem/skyplus/Skyplus.java
@@ -6,12 +6,12 @@ import net.fabricmc.api.ClientModInitializer;
 public class Skyplus implements ClientModInitializer {
     private static Skyplus INSTANCE;
     public static final String NAMESPACE = "skyplus";
-    //LOL
 
     @Override
     public void onInitializeClient() {
         INSTANCE = this;
         ConfigManager.init();
+        int a = 5;
     }
 
     public static Skyplus getInstance() {
