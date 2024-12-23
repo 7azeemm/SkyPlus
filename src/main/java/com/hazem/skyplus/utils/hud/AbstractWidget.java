0a25f6851d5708536b810059a0e9c8ac089dc73e
7azeemm <hazemfarah26@gmail.com>
2024-12-23 19:43:06 +0100

    Improved Widgets Position

--- src/main/java/com/hazem/skyplus/utils/hud/AbstractWidget.java
+++ src/main/java/com/hazem/skyplus/utils/hud/AbstractWidget.java
@@ -3,6 +3,7 @@ package com.hazem.skyplus.utils.hud;
 import com.hazem.skyplus.utils.hud.components.Component;
 import com.hazem.skyplus.utils.hud.components.ComponentBuilder;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
@@ -11,8 +12,11 @@ import net.minecraft.text.Text;
  * Represents a HUD Widget that can display various components (e.g., text, icons) on the screen.
  */
 public abstract class AbstractWidget {
+    private static final MinecraftClient CLIENT = MinecraftClient.getInstance();
     private static final int BACKGROUND_COLOR = 0xc00c0c0c; // Widget's background color.
     private static final int PADDING = 4; // Padding around and within the widget.
+    private static final int BASE_WIDTH = 1366;
+    private static final int BASE_HEIGHT = 768;
     private final ObjectArrayList<Component> components = new ObjectArrayList<>();
     private int width, height;
 
@@ -68,11 +72,11 @@ public abstract class AbstractWidget {
      */
     final void recalculateSize() {
         height = PADDING / 2; // Initialize with top padding.
-        width = PADDING / 2; // Initialize with minimal width for padding.
+        width = 0;
 
         for (Component component : components) {
             height += component.getHeight() + PADDING; // Add component height and padding.
-            width = Math.max(width, component.getWidth() + PADDING / 2); // Adjust to the widest component.
+            width = Math.max(width, component.getWidth() + PADDING - 1); // Adjust to the widest component.
         }
     }
 
@@ -104,8 +108,19 @@ public abstract class AbstractWidget {
     }
 
     final void render(DrawContext context) {
-        int x = getX();
-        int y = getY();
+        // Get the current window size
+        int windowWidth = CLIENT.getWindow().getWidth();
+        int windowHeight = CLIENT.getWindow().getHeight();
+        double scaleFactor = CLIENT.getWindow().getScaleFactor();
+
+        // Calculate the x and y positions based on the base resolution and window size
+        int x = (int) Math.round((getX() / scaleFactor) * windowWidth / BASE_WIDTH);
+        int y = (int) Math.round((getY() / scaleFactor) * windowHeight / BASE_HEIGHT);
+
+        // Ensure the widget doesn't go off the right edge of the window
+        if ((x + width) * scaleFactor > windowWidth) {
+            x = (int) Math.ceil((windowWidth - (width * scaleFactor)) / scaleFactor); // Adjust x so the widget fits within the window width
+        }
 
         // Draw the background if enabled.
         if (shouldDrawBackground()) {
